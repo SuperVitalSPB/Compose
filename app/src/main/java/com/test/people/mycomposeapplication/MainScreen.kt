f8b82b5277b97b6a45dcc161d9045a9cce35da67
Виталий Оанца <o_vital@mail.ru>
2025-04-19 10:48:19 +0300

    BR part two

--- app/src/main/java/com/test/people/mycomposeapplication/MainScreen.kt
+++ app/src/main/java/com/test/people/mycomposeapplication/MainScreen.kt
@@ -1,87 +1,32 @@
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.selection.toggleable
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Add
-import androidx.compose.material.icons.filled.Clear
-import androidx.compose.material.icons.filled.Favorite
-import androidx.compose.material.icons.filled.Info
 import androidx.compose.material.icons.filled.Menu
-import androidx.compose.material.icons.filled.Search
-import androidx.compose.material3.AlertDialog
-import androidx.compose.material3.BottomAppBar
-import androidx.compose.material3.Button
-import androidx.compose.material3.Checkbox
 import androidx.compose.material3.DrawerValue
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
-import androidx.compose.material3.ListItem
-import androidx.compose.material3.ListItemDefaults
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.ModalDrawerSheet
 import androidx.compose.material3.ModalNavigationDrawer
 import androidx.compose.material3.NavigationDrawerItem
 import androidx.compose.material3.NavigationDrawerItemDefaults
-import androidx.compose.material3.OutlinedTextField
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Snackbar
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
-import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.sp
-import androidx.compose.material3.rememberDrawerState
-import androidx.compose.material3.rememberModalBottomSheetState
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.semantics.Role
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import com.test.people.mycomposeapplication.ui.theme.MyComposeApplicationTheme
 import kotlinx.coroutines.launch
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MyComposeApplicationTheme {
-        Greeting("Android")
-    }
-}
-
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MainScreen(modifier: Modifier = Modifier) {
     val items = listOf("Home", "Contact", "About")
     val selectedItem = remember { mutableStateOf(items[0]) }
     val drawerState = rememberDrawerState(DrawerValue.Closed)
     val scope = rememberCoroutineScope()
-    val openDialog = remember { mutableStateOf(false) }
     val textHome = remember { mutableStateOf(items[0]) }
-    val showBottomScheet = remember { mutableStateOf(false) }
 
     ModalNavigationDrawer(
         modifier = modifier,
@@ -98,7 +43,6 @@ fun MainScreen(modifier: Modifier = Modifier) {
                         onClick = {
                             scope.launch { drawerState.close() }
                             selectedItem.value = item
-                            openDialog.value = true
                         },
                         colors = NavigationDrawerItemDefaults.colors(
                             selectedContainerColor = Color.Transparent,
@@ -121,6 +65,8 @@ fun MainScreen(modifier: Modifier = Modifier) {
             }
         }
     )
+}
+/*
     if (openDialog.value) {
         AlertDialog(
             onDismissRequest = { openDialog.value = false},
@@ -148,153 +94,4 @@ fun MainScreen(modifier: Modifier = Modifier) {
             }
         )
     }
-}
-
-@OptIn(ExperimentalMaterial3Api::class)
-@Composable
-fun ModalBottomSheetSample() {
-    var openBottomSheet by rememberSaveable { mutableStateOf(false) }
-    var skipPartiallyExpanded by rememberSaveable { mutableStateOf(false) }
-    val scope = rememberCoroutineScope()
-    val bottomSheetState =
-        rememberModalBottomSheetState(skipPartiallyExpanded = skipPartiallyExpanded)
-
-    // App content
-    Column(
-        horizontalAlignment = Alignment.Start,
-        verticalArrangement = Arrangement.spacedBy(4.dp)
-    ) {
-        Row(
-            Modifier.toggleable(
-                value = skipPartiallyExpanded,
-                role = Role.Checkbox,
-                onValueChange = { checked -> skipPartiallyExpanded = checked }
-            )
-        ) {
-            Checkbox(checked = skipPartiallyExpanded, onCheckedChange = null)
-            Spacer(Modifier.width(16.dp))
-            Text("Skip partially expanded State")
-        }
-        Button(
-            onClick = { openBottomSheet = !openBottomSheet },
-            modifier = Modifier.align(Alignment.CenterHorizontally)
-        ) {
-            Text(text = "Show Bottom Sheet")
-        }
-    }
-
-    // Sheet content
-    if (openBottomSheet) {
-
-        ModalBottomSheet(
-            onDismissRequest = { openBottomSheet = false },
-            sheetState = bottomSheetState,
-        ) {
-            Row(Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.Center) {
-                Button(
-                    // Note: If you provide logic outside of onDismissRequest to remove the sheet,
-                    // you must additionally handle intended state cleanup, if any.
-                    onClick = {
-                        scope
-                            .launch { bottomSheetState.hide() }
-                            .invokeOnCompletion {
-                                if (!bottomSheetState.isVisible) {
-                                    openBottomSheet = false
-                                }
-                            }
-                    }
-                ) {
-                    Text("Hide Bottom Sheet")
-                }
-            }
-            var text by remember { mutableStateOf("") }
-            OutlinedTextField(
-                value = text,
-                onValueChange = { text = it },
-                modifier = Modifier.padding(horizontal = 16.dp),
-                label = { Text("Text field") }
-            )
-            LazyColumn {
-                items(25) {
-                    ListItem(
-                        headlineContent = { Text("Item $it") },
-                        leadingContent = {
-                            Icon(
-                                Icons.Default.Favorite,
-                                contentDescription = "Localized description"
-                            )
-                        },
-                        colors =
-                            ListItemDefaults.colors(
-                                containerColor = MaterialTheme.colorScheme.surfaceContainerLow
-                            ),
-                    )
-                }
-            }
-        }
-    }
-}
-
-
-@Composable
-fun SchowSnackbar() {
-    Snackbar{
-        Text("Загрузка завершена", fontSize = 22.sp)
-    }
-}
-
-@Composable
-fun HomeScreenLesson22() {
-    val isAdded = remember { mutableStateOf(false) }
-    val isShowSnackbar = remember { mutableStateOf(false) }
-    val scope = rememberCoroutineScope()
-
-    Scaffold (
-        topBar = {
-            @OptIn(ExperimentalMaterial3Api::class)
-            TopAppBar(title= { Text("METANIT.COM", fontSize = 22.sp)},
-                navigationIcon={ IconButton({ }) { Icon(Icons.Filled.Menu, contentDescription = "Меню")}},
-                actions={
-                    IconButton({
-                        isAdded.value = !isAdded.value
-                    }) { Icon(Icons.Filled.Info, contentDescription = "О приложении")}
-                    IconButton(onClick = {
-                        isShowSnackbar.value = !isShowSnackbar.value
-                    }) {Icon(Icons.Filled.Search, contentDescription = "Поиск")}
-                },
-                colors= TopAppBarDefaults.topAppBarColors(containerColor = Color.DarkGray,
-                    titleContentColor = Color.LightGray,
-                    navigationIconContentColor = Color.LightGray,
-                    actionIconContentColor = Color.LightGray))
-        },
-        bottomBar = {
-            BottomAppBar(
-                containerColor = Color.DarkGray,
-                contentColor = Color.LightGray
-            ){
-                IconButton(onClick = {  }) { Icon(Icons.Filled.Favorite, contentDescription = "Избранное")}
-                Spacer(Modifier.weight(1f, true))
-                IconButton(onClick = {  }) { Icon(Icons.Filled.Info, contentDescription = "Справка")}
-            }
-        },
-        floatingActionButton = {
-            FloatingActionButton(
-                content = {
-                    if(isAdded.value) Icon(Icons.Filled.Clear, contentDescription = "Удалить")
-                    else Icon(Icons.Filled.Add, contentDescription = "Добавить") },
-                onClick = { isAdded.value = !isAdded.value}
-            )
-        },
-        snackbarHost = {
-            if (isShowSnackbar.value) {
-                Snackbar {
-                    Text("Загрузка завершена", fontSize = 22.sp)
-                }
-            }
-        }
-
-    ){
-        Text("Hello METANIT.COM", fontSize = 28.sp, modifier = Modifier.padding(it))
-    }
-
-}
+*/
